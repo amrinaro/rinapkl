3432fbab1b7f068bad5993eb1d8d68bc5a676c2e
amrinaro <124320734+amrinaro@users.noreply.github.com>
2023-02-03 12:04:23 +0700

    Add files via upload

---
+++ info.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>selamat datang</title>
+</head>
+<body>
+	<h2 align="center">HIDAYATULLAH KUDUS</h2>
+	<div style="padding: 10px"></div>
+	<?php 
+	
+	echo "<img src='smk.jpg' width='500' height='300' 
+		style='margin:10px' />";
+		?>
+		
+		<h3>Visi & Misi</h3>
+		<p>Visi  “Terwujudnya generasi muslim yang islami”</p>
+		<p>Misi  “Menyelenggarakan pendidikan yang memadukan aspek pemahaman islam, ilmu pengetahuan , dan ketrampilan.”</p>
+	
+
+		<a href="index.php">link.com</a>
+</body>
+</html>
